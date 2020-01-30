008b687ca4fb418466bf02dcdb4aed88a889b070
admin <rootaka@gmail.com>
2020-01-30 11:42:16 +0900

    1st commit

---
+++ docs/precache-manifest.143fd7a5fc717a55d98597abdbaace5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4168569963b4b6b2e1accd0144e83bfe",
+    "url": "/start-react/index.html"
+  },
+  {
+    "revision": "6901a3cc8c3a0cb64163",
+    "url": "/start-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4d294df4aa5206d0b37",
+    "url": "/start-react/static/js/2.a73977fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/start-react/static/js/2.a73977fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6901a3cc8c3a0cb64163",
+    "url": "/start-react/static/js/main.f5fcfd68.chunk.js"
+  },
+  {
+    "revision": "ba53917003f5fa468639",
+    "url": "/start-react/static/js/runtime-main.cd28f782.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/start-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
